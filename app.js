92eefd2bc5ee2df176b625e8d7fc7c034ecb5c17
tshur <tshur@webzavod.ru>
2017-01-13 15:45:05 +0400

    первый коммит

---
+++ app.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var answer = 'YES';
+
+var serverSite = http.createServer(function(req, res) {
+  res.writeHead(200);
+  if (req.method == 'POST'){
+  if (req.url == '/YES'){
+  answer='YES';
+  }else{
+  answer='NO';}
+
+  }
+   var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<p><b>Submit button:</b><Br>'+
+    '<form action= "/YES" method="post">'+
+   '<input type="submit" name="YES" value="YES"> '+
+      '</form>'+
+	'<form action= "/NO" method="post">'+
+   '<input type="submit" name="NO" value="NO"> '+
+    '</form>'+
+    '<p> You answer is '+ answer+'</p>'+
+      
+    '</body>'+
+    '</html>';
+
+  res.end(body);
+});
+
+serverSite.listen(8089);
