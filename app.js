2c1d0227b826085b91bafd35029e1d6203d46a48
Shurpanchik <Shur1993@mail.ru>
2017-02-01 09:35:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ server.post('/api/messages', connector.listen());
 bot.dialog('/', function (session) {
    session.send("Hello, button");
    
-builder.Prompts.choice(session,"Choose time","09:00|10:00|11:00|12:00|13:00|14:00|15:00|16:00|17:00|18:00|19:00|20:00" , {
+builder.Prompts.choice(session,"Choose time","09:00|10:00|11:00|12:00|13:00|14:00|15:00|16:00|17:00|18:00|19:00" , {
 listStyle: builder.ListStyle.button
 });
 
