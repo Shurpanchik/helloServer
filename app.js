e55d79e5b950f6da061c11d6309e86f9c9675de8
Shurpanchik <Shur1993@mail.ru>
2017-07-20 14:27:05 +0400

    Update app.js
    отправление выбранного времени

--- app.js
+++ app.js
@@ -107,6 +107,10 @@ bot.dialog('/start', [
 		builder.Prompts.choice(session,"Choose time","09:00|10:00|11:00|12:00|13:00|14:00|15:00|16:00|17:00|18:00|19:00|20:00" , {
 listStyle: builder.ListStyle.button,
 });
+    },
+	 function (session, results) {
+	session.send(results.response.entity);
+	session.endDialog();
     }
 ]);
 bot.dialog('/menu', [
